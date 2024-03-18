27a99758d60b7856b08d046617910de344171c67
LucaSantosSP <lucas-santos20.17@hotmail.com>
2024-03-17 22:50:27 -0300

    Iniciado construção do card de alimentos

--- pages/refeicao/Refeicao.js
+++ pages/refeicao/Refeicao.js
@@ -1,29 +1,40 @@
 import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Text, View, Button, ScrollView } from 'react-native';
 import NavigationButtons from '../../util/NavBar';
 import { useNavigation } from '@react-navigation/native';
+import refeicaoStyle from './RefeicaoStyle';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Refeicao({ route }) {
   const navigation = useNavigation();
   const { key } = route.params;
 
   return (
-    <View style={{ flex: 1 }}>
+    <ScrollView style={{ flex: 1 }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>{key}!</Text>
-      </View>
-      <View style={styles.navigationContainer}>
-        <NavigationButtons navigation={navigation} />
-      </View>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  navigationContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
-});
+        <View style={refeicaoStyle.containerBranco}>
+          <View style={refeicaoStyle.table}>
+            <View style={refeicaoStyle.firstRow}>
+              <Text style={refeicaoStyle.firstCell}>Lista de alimentos</Text>
+            </View>
+            <View style={refeicaoStyle.row}>
+              <Text style={refeicaoStyle.cellLeft}>Ovo</Text>
+              <Text style={[refeicaoStyle.cellRigth, refeicaoStyle.corTitulo]}> Mais </Text>
+            </View>
+          </View>
+        </View>
+
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+
+        <View style={refeicaoStyle.navigationContainer}>
+          <NavigationButtons navigation={navigation} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
