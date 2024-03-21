108392a85d619397a3275f6c16c4d7ab31957e12
LucaSantosSP <lucas-santos20.17@hotmail.com>
2024-03-21 03:12:31 -0300

    Adicionado valores de macronutrientes

--- services/UsuarioService.js
+++ services/UsuarioService.js
@@ -112,6 +112,24 @@ class UsuarioService {
             return Promise.reject(error);
         }
     }
+
+    async getMacronutrientes() {
+        try {
+            console.log(this.token)
+            console.log(this.cdUsuario)
+            const response = await axios.get(`${this.baseURL}/refeicao/macronutrientes/${this.cdUsuario}`, {
+                timeout: this.timeout,
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${this.token}`
+                }
+            });
+
+            return response.data;
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
 }
 
 const usuarioService = new UsuarioService();
