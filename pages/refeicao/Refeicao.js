a8b087fe2626501e2e716fd34d22f755d365fb62
LucaSantosSP <lucas-santos20.17@hotmail.com>
2024-03-20 21:32:46 -0300

    Adiconado botão de novo alimento na refeição

--- pages/refeicao/Refeicao.js
+++ pages/refeicao/Refeicao.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, View, Button, ScrollView } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import NavigationButtons from '../../util/NavBar';
 import { useNavigation } from '@react-navigation/native';
 import refeicaoStyle from './RefeicaoStyle';
+import { Button } from 'react-native-elements';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Refeicao({ route }) {
@@ -22,7 +23,7 @@ export default function Refeicao({ route }) {
               
               <View style={refeicaoStyle.row}>
                 <View style={refeicaoStyle.cellLeft}>
-                  <Text style={refeicaoStyle.firstTextCellLeft}>Ovo</Text>
+                  <Text style={refeicaoStyle.firstTextCellLeft}>Ovo cozido</Text>
                   <Text style={refeicaoStyle.secondTextCellLeft}>100 gramas - 541 Kcal</Text>
                 </View>
                 <View style={[refeicaoStyle.cellRigth, refeicaoStyle.corTitulo]}>
@@ -30,6 +31,18 @@ export default function Refeicao({ route }) {
                   <Text></Text>
                 </View>
               </View>
+
+              <Button title="Adicionar seu alimento" 
+              containerStyle={{
+                width: 200,
+                marginHorizontal: 50,
+                marginVertical: 10,
+                marginTop: 30,
+              }}
+              buttonStyle={{
+                backgroundColor: 'rgba(8, 69, 80, 1)',
+                borderRadius: 8,
+              }}/>
 
             </View>
           </View>
