8391dec7a5fb0ce77e89fbbc50cd24741d5e0801
LucaSantosSP <lucas-santos20.17@hotmail.com>
2024-04-10 23:44:24 -0300

    Botão de menu retorna ao menu

--- util/NavBar.js
+++ util/NavBar.js
@@ -3,25 +3,36 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
 
 class NavigationButtons extends Component {
-  render() {
-    const { navigation } = this.props;
+  handleNavigation = (screenName) => {
+    const { currentScreen, navigation } = this.props;
 
+    // Verifique se a tela a ser navegada é diferente da tela atual
+    if (currentScreen !== screenName) {
+      // Reset a pilha de navegação e navegue para a tela especificada
+      navigation.reset({
+        index: 0,
+        routes: [{ name: screenName }],
+      });
+    }
+  };
+
+  render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', backgroundColor: 'white', borderTopWidth: 1, borderTopColor: '#e1e1e1' }}>
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '80%', marginTop: 5 }}>
-          <TouchableOpacity onPress={() => navigation.navigate('Menu')} style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <TouchableOpacity onPress={() => this.handleNavigation('Menu')} style={{ flexDirection: 'column', alignItems: 'center' }}>
             <MaterialCommunityIcons name="home" color="#8b8b8b" size={25} style={{ margin: 0, padding: 0 }} />
             <Text style={{ marginLeft: 5, marginTop: 0, padding: 0 }}></Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('AlimentoPesquisa')} style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <TouchableOpacity onPress={() => this.handleNavigation('AlimentoPesquisa')} style={{ flexDirection: 'column', alignItems: 'center' }}>
             <MaterialCommunityIcons name="plus" color="#8b8b8b" size={25} style={{ margin: 0, padding: 0 }} />
             <Text style={{ marginLeft: 5, margin: 0, padding: 0 }}></Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Historico')} style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <TouchableOpacity onPress={() => this.handleNavigation('Historico')} style={{ flexDirection: 'column', alignItems: 'center' }}>
             <MaterialCommunityIcons name="calendar" color="#8b8b8b" size={25} style={{ margin: 0, padding: 0 }} />
             <Text style={{ marginLeft: 5, margin: 0, padding: 0 }}></Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Perfil')} style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <TouchableOpacity onPress={() => this.handleNavigation('Perfil')} style={{ flexDirection: 'column', alignItems: 'center' }}>
             <MaterialCommunityIcons name="account" color="#8b8b8b" size={25} style={{ margin: 0, padding: 0 }} />
             <Text style={{ marginLeft: 5, margin: 0, padding: 0 }}></Text>
           </TouchableOpacity>
